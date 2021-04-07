b382bff77a9661d289c43483751e65e90ede38cc
Yuhang <77423695+yuhang-eng@users.noreply.github.com>
2021-04-07 22:04:51 +0800

    auto refresh AddressBar

--- src/components/AddressBar.js
+++ src/components/AddressBar.js
@@ -1,31 +1,61 @@
-import React, { useCallback, useEffect } from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
+import Web3 from "web3";
 
 
 const style={
-    background: "#61dafb"
+    background: "#ff7744"
+}
+
+const buttonStyle={
+    background: "428bca",
 }
 
 function AddressBar (props) {
 
-  useEffect( ()=>{ 
+  const [currentAccount,setCurrentAccount]=useState(null)
+  const deployedContract = props.deployedContract
+  
+
+  useEffect( ()=>{
+    let timer
     (async ()=>{
-      const deployedContract= props.deployedContract
-      if(deployedContract){
+      await getWeb3ProviderAndWebSocket()
+      await connectToContract()
 
-          console.log()
+      timer= setInterval(connectToContract,1000)
+      
+    })()
+  },[])
 
-      }
+  async function getWeb3ProviderAndWebSocket(){
+    if (window.ethereum) {
+      window.web3 = new Web3(window.ethereum);
+      await window.ethereum.enable();
+    }
+    else if (window.web3) {
+      window.web3 = new Web3(window.web3.currentProvider);
+    }
+    else {
+      window.alert('Non-Ethereum browser detected. You should consider trying MetaMask!');
+    }
+    window.web3Socket = new Web3("wss://rinkeby.infura.io/ws/v3/4770b93234ff418e8c15c8f67b45d41a");
+    if(!window.web3Socket){
+      window.alert('Please make sure your wss copied from Infura is correct.')
+    }
+  }
 
-  })()
-  },[props.deployedContract])
+  async function connectToContract(){
+    const web3 = window.web3;
+    const accounts = await web3.eth.getAccounts();
+    setCurrentAccount(accounts[0]);
+  }  
 
   let checkUserSeeds = useCallback (async()=>{
-    const deployedContract=props.deployedContract
 
     console.log("your personal SeedsID")
     let seedArray = []
 
-    let counter = await deployedContract.methods.uploadAndDownloadCounter(props.account).call()
+    let counter = await deployedContract.methods.uploadAndDownloadCounter(currentAccount).call()
 
     // var len = 0
     // for (var j in counter)
@@ -35,7 +65,7 @@ function AddressBar (props) {
     console.log(counter)
 
     for (let i=0; i<counter; i++) {
-      let personalSeeds = await deployedContract.methods.OwnerSeedsSummary(props.account,i).call()
+      let personalSeeds = await deployedContract.methods.OwnerSeedsSummary(currentAccount,i).call()
       seedArray.push(personalSeeds)
     }
 
@@ -51,12 +81,11 @@ function AddressBar (props) {
     <nav style={style}>
         <ul className="navbar-nav px-3">
             <li className="nav-item text-nowrap d-none d-sm-none d-sm-block">
-            <button onClick={checkUserSeeds}>
-                <small className="text-black"><span id="account">{"Your Address: " + props.account}</span></small>
-            </button>
+            <small className="text-black"><span id="account">{"Your Address:" + currentAccount}</span></small>
+            <button style={buttonStyle} onClick={checkUserSeeds}>Personal Seeds</button>
             </li>
         </ul>
-    </nav>
+    </nav> 
     );
 }
 
