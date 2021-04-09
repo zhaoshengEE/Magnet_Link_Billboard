52bd6733961891d13ced33a9dcb853f416f9f242
zhaoshengEE <jerryli20160808@gmail.com>
2021-04-08 18:18:32 -0700

    Update the table with color changing functionality

--- src/components/LinkItem.js
+++ src/components/LinkItem.js
@@ -1,14 +1,20 @@
-import React from 'react';
+import React, {useState} from 'react';
+
+const azure = '#f0ffff';
+const lightgreen = '#90ee90';
 
 const SeedItem = (props) => {
-    const seedInfo=props.seedInfo
+    const seedInfo=props.seedInfo;
+    const [color, setColor] = useState(azure);
+    const newColor = color == azure ? lightgreen : azure;
+
     return (
-        <tr>
-            <td> {seedInfo.seedId} </td>
-            <td> name: {seedInfo.seedName} </td>
-            <td> keyWords: {seedInfo.keyWords} </td>
-            <td> seedDescription: {seedInfo.seedDescription} </td>
-            <td> endorseAmount: {seedInfo.endorseAmount} </td>
+        <tr style={{backgroundColor: color}}>
+            <td onClick={()=>setColor(newColor)}> {seedInfo.seedId} </td>
+            <td onClick={()=>setColor(newColor)}> name: {seedInfo.seedName} </td>
+            <td onClick={()=>setColor(newColor)}> keyWords: {seedInfo.keyWords} </td>
+            <td onClick={()=>setColor(newColor)}> seedDescription: {seedInfo.seedDescription} </td>
+            <td onClick={()=>setColor(newColor)}> endorseAmount: {seedInfo.endorseAmount} </td>
 
 
         </tr>
